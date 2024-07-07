c9b889aa589e65547ffb51aa303477c44013c434
Nicolae Nicora <nicolae.nicora@yahoo.com>
2024-07-07 16:35:35 +0200

    document the time

--- src/time/time.zig
+++ src/time/time.zig
@@ -139,66 +139,78 @@ pub const Time = struct {
         return @as(i32, @bitCast(loc.Lookup().offset));
     }
 
-    // format returns a date with custom format
-    // | | Token | Output |
-    // |-----------------:|:------|:---------------------------------------|
-    // | Month
-    // | | M  | 1 2 ... 11 12 |
-    // | | Mo | 1st 2nd ... 11th 12th |
-    // | | MM | 01 02 ... 11 12 |
-    // | | MMM | Jan Feb ... Nov Dec |
-    // | | MMMM | January February ... November December |
-    // | Quarter
-    // | | Q  | 1 2 3 4 |
-    // | | QQ | 01 02 03 04 |
-    // | | Qo | 1st 2nd 3rd 4th |
-    // | Day of Month
-    // | | D  | 1 2 ... 30 31 |
-    // | | Do | 1st 2nd ... 30th 31st |
-    // | | DD | 01 02 ... 30 31 |
-    // | Day of Year
-    // | |  DDD | 1 2 ... 364 365 |
-    // | | DDDo | 1st 2nd ... 364th 365th |
-    // | | DDDD | 001 002 ... 364 365 |
-    // | Day of Week
-    // | | d | 0 1 ... 5 6 (Sun-Sat) |
-    // | | c | 1 2 ... 6 7 (Mon-Sun) |
-    // | | dd | Su Mo ... Fr Sa |
-    // | | ddd | Sun Mon ... Fri Sat |
-    // | | dddd | Sunday Monday ... Friday Saturday |
-    // | Week of Year
-    // | | w  | 1 2 ... 52 53 |
-    // | | wo | 1st 2nd ... 52nd 53rd |
-    // | | ww | 01 02 ... 52 53 |
-    // | Year
-    // | |   YY | 70 71 ... 29 30 |
-    // | | YYYY | 1970 1971 ... 2029 2030 |
-    // | Era
-    // | | N  | BC AD |    - AD (Not yet supported)
-    // | | NN | Before Christ, Anno Domini |    - AD (Not yet supported)
-    // | AM/PM
-    // | | A | AM PM |
-    // | | a | am pm |
-    // | Hour
-    // | | H  | 0 1 ... 22 23 |
-    // | | HH | 00 01 ... 22 23 |
-    // | | h | 1 2 ... 11 12 |
-    // | | hh | 01 02 ... 11 12 |
-    // | | k | 1 2 ... 23 24 |
-    // | | kk | 01 02 ... 23 24 |
-    // | Minute
-    // | | m  | 0 1 ... 58 59 |
-    // | | mm | 00 01 ... 58 59 |
-    // | Second
-    // | | s  | 0 1 ... 58 59 |
-    // | | ss | 00 01 ... 58 59 |
-    // | Offset
-    // | | Z  | -7 -6 ... +5 +6 |
-    // | | ZZ | -0700 -0600 ... +0500 +0600 |
-    // | | ZZZ | -07:00 -06:00 ... +05:00 +06:00 |
-    // Usage:
-    // Time.now().format('MMMM Mo YY N kk:mm:ss A')) // output like: January 1st 22 AD 13:45:33 PM
-
+    /// Format a date to a string according to requested pattern.
+    ///
+    /// Example:
+    /// `Time.now().formatf('MMMM Mo YY N kk:mm:ss A')) -> January 1st 22 AD 13:45:33 PM`
+    ///
+    /// **Month**
+    ///   - `M  -> 1 2 ... 11 12`
+    ///   - `Mo -> 1st 2nd ... 11th 12th`
+    ///   - `MM -> 01 02 ... 11 12`
+    ///   - `MMM -> Jan Feb ... Nov Dec`
+    ///   - `MMMM -> January February ... November December`
+    ///
+    /// **Quarter**
+    ///   - `Q  -> 1 2 3 4`
+    ///   - `QQ -> 01 02 03 04`
+    ///   - `Qo -> 1st 2nd 3rd 4th`
+    ///
+    /// **Day of Month**
+    ///   - `D  -> 1 2 ... 30 31`
+    ///   - `Do -> 1st 2nd ... 30th 31st`
+    ///   - `DD -> 01 02 ... 30 31`
+    ///
+    /// **Day of Year**
+    ///   - `DDD -> 1 2 ... 364 365`
+    ///   - `DDDo -> 1st 2nd ... 364th 365th`
+    ///   - `DDDD -> 001 002 ... 364 365`
+    ///
+    ///  **Day of Week**
+    ///   - `d -> 0 1 ... 5 6 (Sun-Sat)`
+    ///   - `c -> 1 2 ... 6 7 (Mon-Sun)`
+    ///   - `dd -> Su Mo ... Fr Sa`
+    ///   - `ddd -> Sun Mon ... Fri Sat`
+    ///   - `dddd -> Sunday Monday ... Friday Saturday`
+    ///
+    /// **Week of Year**
+    ///   - `w  -> 1 2 ... 52 53`
+    ///   - `wo -> 1st 2nd ... 52nd 53rd`
+    ///   - `ww -> 01 02 ... 52 53`
+    ///
+    /// **Year**
+    ///   - `YY -> 70 71 ... 29 30`
+    ///   - `YYYY -> 1970 1971 ... 2029 2030`
+    ///
+    /// **Era**
+    ///   - `N  -> BC AD`                       //AD (Not yet supported)
+    ///   - `NN -> Before Christ, Anno Domini`  //AD (Not yet supported)
+    ///
+    /// **AM/PM**
+    ///   - `A -> AM PM`
+    ///   - `a -> am pm`
+    ///
+    /// **Hour**
+    ///   - `H  -> 0 1 ... 22 23`
+    ///   - `HH -> 00 01 ... 22 23`
+    ///   - `h  -> 1 2 ... 11 12`
+    ///   - `hh -> 01 02 ... 11 12`
+    ///   - `k  -> 1 2 ... 23 24`
+    ///   - `kk -> 01 02 ... 23 24`
+    ///
+    /// **Minute**
+    ///   - `m  -> 0 1 ... 58 59`
+    ///   - `mm -> 00 01 ... 58 59`
+    ///
+    /// **Second**
+    ///   - `s  -> 0 1 ... 58 59`
+    ///   - `ss -> 00 01 ... 58 59`
+    ///
+    /// **Offset**
+    ///   - `Z   -> -7 -6 ... +5 +6`
+    ///   - `ZZ  -> -0700 -0600 ... +0500 +0600`
+    ///   - `ZZZ -> -07:00 -06:00 ... +05:00 +06:00`
+    ///
     pub fn formatf(self: Self, allocator: std.mem.Allocator, pattern: []const u8, writer: anytype) !void {
         var sb = try self.format(allocator, pattern);
         defer sb.deinit();
@@ -206,6 +218,7 @@ pub const Time = struct {
         _ = try writer.write(sb.bytes());
     }
 
+    /// Format the date and time according to requested pattern to a destination
     pub fn formatfInto(self: Self, allocator: std.mem.Allocator, pattern: []const u8, dst: []const u8) !usize {
         var sb = try Utf8Buffer.initWithCapacity(allocator, pattern.len);
         errdefer sb.deinit();
@@ -440,6 +453,7 @@ pub const Time = struct {
     }
 };
 
+/// Verify if the year is a leap year, which is a number which divide by 4, 100 and 400 having remaining number equal with 0.
 pub inline fn isLeap(year: i128) bool {
     return @rem(year, 4) == 0 and (@rem(year, 100) != 0 or @rem(year, 400) == 0);
 }
