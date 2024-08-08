3616d23591b736e897a2a9c7a21ad89ff157273f
Nicolae NIcora <nicolae.nicora@taryafintech.com>
2024-08-08 14:16:34 +0200

    add the generic comparator for an array of data

--- src/cmp/mod.zig
+++ src/cmp/mod.zig
@@ -1,4 +1,5 @@
 pub const Ordering = @import("types.zig").Ordering;
 pub const Wrapper = @import("wrapper.zig").Wrapper;
+pub const WrapperArray = @import("wrapper.zig").WrapperArray;
 pub const GenericPartialOrd = @import("partial.zig").GenericPartialOrd;
 pub const GenericPartialEq = @import("partial.zig").GenericPartialEq;
--- src/cmp/wrapper.zig
+++ src/cmp/wrapper.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const Ordering = @import("types.zig").Ordering;
 const GenericPartialOrd = @import("partial.zig").GenericPartialOrd;
 
@@ -17,6 +18,33 @@ pub fn Wrapper(comptime T: type) type {
             if (self.value > other.value) return .greater;
             if (self.value < other.value) return .less;
             if (self.value == other.value) return .equal;
+            return null;
+        }
+
+        pub fn cmper(self: *const Self) PartialOrd {
+            return .{ .context = self };
+        }
+    };
+}
+
+pub fn WrapperArray(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        pub const PartialOrd = GenericPartialOrd(*const Self, partial_cmp);
+
+        value: []const T,
+
+        pub fn fromLiteral(v: []const T) Self {
+            return Self{ .value = v };
+        }
+
+        fn partial_cmp(self: *const Self, other: *const Self) ?Ordering {
+            if (self.value.len > other.value.len) return .greater;
+            if (self.value.len < other.value.len) return .less;
+
+            if (std.mem.eql(T, self.value, other.value)) return .equal;
+
             return null;
         }
 
